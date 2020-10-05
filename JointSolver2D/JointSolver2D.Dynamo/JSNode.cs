9d7341e23be3c312567c9acbfa03158a018da482
nniem <nicholasniem@outlook.com>
2020-10-05 09:43:10 +0100

    Dynamo start

---
+++ JointSolver2D/JointSolver2D.Dynamo/JSNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using js = JointSolver2D;
+
+namespace JointSolver2D.Dynamo
+{
+
+    public class JSNode
+    {
+        public static js.JSNode FreeNodeByCoordinates(double x, double y) => new js.JSNode(new Vector2d(x, y));
+    }
+}
